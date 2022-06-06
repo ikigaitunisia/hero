4bfd5dc68804895f9b7609e83b183176b9584398
takwa <takouaguetat@gmail.com>
2022-06-06 14:07:36 +0100

    fix last commit

--- src/components/FeedCapsule.js
+++ src/components/FeedCapsule.js
@@ -1,44 +1,46 @@
 import React from "react";
 import "./ActivistProfileCapsule.css";
-import "./FeedCapsule.css"
+import "./FeedCapsule.css";
 
 function FeedCapsule(props) {
   return (
     <div id="appCapsule">
       <div className="section wallet-card-section pt-1">
-        <div
-          className="wallet-card"
-          style={{ height: "40vh", padding: 0, borderRadius: "15px" }}
-        >
-          <div style={{ width: "70%", padding: "20px 0", margin: "0 auto" }}>
-            <h1>Your investment has global impact.</h1>
-            <p>
-              By investing in the HERO Coin you contribute to providing a
-              Universal Basic Income for a year to a verified mobilizer of your
-              choice with a track record of accelerating climate action.
+        <div className="wallet-card" style={{ borderRadius: "15px" }}>
+          <div
+            style={{ width: "70%", padding: "20px 0", margin: "0 auto" }}
+            className="custom-font"
+          >
+            <h1 className="blue-text">Your investment has global impact.</h1>
+            <hr data-content="AND" class="hr-text"></hr>
+            <p style={{ color: "black" }}>
+              By investing in the <b>HERO Coin</b> you contribute to providing a
+              Universal <b>Basic Income</b> for a year to a verified{" "}
+              <b>mobilizer</b> of your choice with a track record of
+              accelerating climate action.
             </p>
-            <p>
-              This will allow the mobilizer to dedicate full time to their
-              climate work and persue global policies to keep the world’s
-              temperature below 1.5 degrees, the minimum required for all of us
-              to live in an habitable world.
+            <p style={{ color: "black" }}>
+              This will allow the mobilizer to <b>dedicate full time</b> to
+              their climate work and persue global policies to keep the{" "}
+              <b>world’s temperature below 1.5 degrees</b>, the minimum required
+              for all of us to live in an habitable world.
             </p>
           </div>
         </div>
       </div>
-      <div className="section mt-4">
+      <div className="center-content">
         <ion-icon
-          class="icon-title"
+          style={{ width: "200px", height: "100px" }}
           src="assets/img/svg/heroMobilizers.svg"
         ></ion-icon>
       </div>
-      <div className="section">
+      <div className="section mb-2">
         <div className="row mt-2">
           <div className="col-12">
             <div className="stat-box box-bg">
               <div className="custom-font">
-                <h1 className="blue-text">Anuna de Wever</h1>
-                <span>Belgium</span>
+                <h1 className="white-text">Anuna de Wever</h1>
+                <span className="white-text">Belgium</span>
               </div>
               <div className="btn-section">
                 <button
