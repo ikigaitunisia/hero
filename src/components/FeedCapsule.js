6fc4ce712e6eedd0def3927573adc7680a4cccef
takwa <takouaguetat@gmail.com>
2022-06-06 14:20:54 +0100

    fix last commit

--- src/components/FeedCapsule.js
+++ src/components/FeedCapsule.js
@@ -1,25 +1,21 @@
 import React from "react";
-import "./ActivistProfileCapsule.css";
 import "./FeedCapsule.css";
 
 function FeedCapsule(props) {
   return (
     <div id="appCapsule">
       <div className="section wallet-card-section pt-1">
-        <div className="wallet-card" style={{ borderRadius: "15px" }}>
-          <div
-            style={{ width: "70%", padding: "20px 0", margin: "0 auto" }}
-            className="custom-font"
-          >
+        <div className="wallet-card">
+          <div className="custom-font center-div">
             <h1 className="blue-text">Your investment has global impact.</h1>
             <hr data-content="AND" class="hr-text"></hr>
-            <p style={{ color: "black" }}>
+            <p className="black-text">
               By investing in the <b>HERO Coin</b> you contribute to providing a
               Universal <b>Basic Income</b> for a year to a verified{" "}
               <b>mobilizer</b> of your choice with a track record of
               accelerating climate action.
             </p>
-            <p style={{ color: "black" }}>
+            <p className="black-text">
               This will allow the mobilizer to <b>dedicate full time</b> to
               their climate work and persue global policies to keep the{" "}
               <b>world’s temperature below 1.5 degrees</b>, the minimum required
@@ -30,7 +26,7 @@ function FeedCapsule(props) {
       </div>
       <div className="center-content">
         <ion-icon
-          style={{ width: "200px", height: "100px" }}
+          class="hero-mobilizers-logo"
           src="assets/img/svg/heroMobilizers.svg"
         ></ion-icon>
       </div>
@@ -43,16 +39,10 @@ function FeedCapsule(props) {
                 <span className="white-text">Belgium</span>
               </div>
               <div className="btn-section">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-lg rounded shadowed"
-                >
+                <button type="button" class="btn btn-primary rounded shadowed">
                   See Profile
                 </button>
-                <button
-                  type="button"
-                  class="btn btn-primary btn-lg rounded shadowed"
-                >
+                <button type="button" class="btn btn-primary rounded shadowed">
                   Support
                 </button>
               </div>
