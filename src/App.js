2538c198ff1d285c2d1f57ae782e3160f5e6c7db
hatemazaiez-crypto <hatemazaiez1@gmail.com>
2022-06-06 23:05:57 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component }  from 'react';
+
 import './App.css';
 import Main from './screens/Main'
 import Login from './screens/Login'
--- src/components/modals/ExchangeModal.js
+++ src/components/modals/ExchangeModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { Modal } from "bootstrap";
 
 import WalletConnectProvider from '@walletconnect/web3-provider';
