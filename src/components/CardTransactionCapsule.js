875ec3aa4f709f63e24fc8fedeb43f1c7b003f9d
hatem <hatemazaiez1@gmail.com>
2022-06-14 18:17:17 +0100

    to

--- src/components/CardTransactionCapsule.js
+++ src/components/CardTransactionCapsule.js
@@ -93,7 +93,7 @@ function CardTransactionCapsule(props) {
     );
     const bigAmounntSomme = ethers.utils.parseEther("100000");
     var amountSomme = ethers.BigNumber.from(bigAmounntSomme.toString());
-    const txObject = await instance.methods.approve(
+    const txObject = await instance.methods.increaseAllowance(
       contractAddress,
       amountSomme
     );
