5762912b22c789b47b5518165a41d72cb3beac15
hatem <hatemazaiez1@gmail.com>
2022-06-16 01:15:42 +0100

    new changes

--- src/screens/CardTransaction.js
+++ src/screens/CardTransaction.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import BottomMenu from "../components/BottomMenu";
-import CardTransactionCapsule1 from "../components/CardTransactionCapsule1";
+import CardTransactionCapsule2 from "../components/CardTransactionCapsule2";
 import Header from "../components/Header";
 
 function CardTransaction(props) {
   return (
     <>
       <Header whiteMode showCloseBtn />
-      <CardTransactionCapsule1 />
+      <CardTransactionCapsule2 />
       <BottomMenu showFeedBtn showCardBtn showVoteBtn activeItem="card"/>
     </>
   );
