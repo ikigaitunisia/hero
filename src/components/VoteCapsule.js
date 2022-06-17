8006140ef590a4f90cd94195e3b96ea33ea04bf2
takwa <takouaguetat@gmail.com>
2022-06-17 09:53:57 +0100

    fix vote page

--- src/components/VoteCapsule.js
+++ src/components/VoteCapsule.js
@@ -4,15 +4,19 @@ import "./VoteCapsule.css";
 function VoteCapsule(props) {
   return (
     <div id="appCapsule" className="bg-g" style={{ minHeight: "100vh" }}>
-      <div className="section custom-width center-div-2 white-text mt-4 mb-4">
-        <h2 className="white-text">
+      <div className="section custom-width center-div-2 white-text mb-4">
+        <h2 className="white-text text-title">
           You have the power to vote on the future of HERO.
         </h2>
         <p>
           As a HERO Coin holder you have a say in how the community moves
           forward.
         </p>
-        <a href="" className="white-text">
+        <a
+          href=""
+          className="white-text"
+          style={{ textDecoration: "underline" }}
+        >
           Discover how voting works.
         </a>
       </div>
@@ -30,7 +34,7 @@ function VoteCapsule(props) {
           <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 flex-center mb-2">
             <button
               type="button"
-              class="btn btn-icon rounded me-1"
+              className="btn btn-icon rounded me-1"
               style={{
                 backgroundColor: "black",
                 width: "50px",
@@ -42,14 +46,19 @@ function VoteCapsule(props) {
                 style={{ color: "white", display: "flex" }}
               ></ion-icon>
             </button>
-            <div className="">
-              <span class="badge badge-dark custom-badge">20%</span>
+            <div>
+              <span
+                className="badge badge-dark custom-badge"
+                style={{ borderRadius: "30px" }}
+              >
+                20%
+              </span>
             </div>
           </div>
           <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 flex-center mb-2">
             <button
               type="button"
-              class="btn btn-icon rounded me-1"
+              className="btn btn-icon rounded me-1"
               style={{
                 backgroundColor: "white",
                 width: "50px",
@@ -62,31 +71,47 @@ function VoteCapsule(props) {
               ></ion-icon>
             </button>
             <div>
-              <span class="badge custom-badge-white">80%</span>
+              <span
+                className="badge custom-badge-white"
+                style={{ borderRadius: "30px" }}
+              >
+                80%
+              </span>
             </div>
           </div>
         </div>
       </div>
       <div className="section center-div-2 mb-4 pb-4">
-        <h3 className="white-text">Voting ends in:</h3>
+        <h3 className="white-text" style={{ textDecoration: "underline" }}>
+          Voting ends in:
+        </h3>
         <div className="row wallet-card custom-padding mt-3">
-          <div className="col-3" style={{ borderRight: "1px solid" }}>
+          <div
+            className="col-3 center-content"
+            style={{ borderRight: "1px solid" }}
+          >
             <h1 className="small-font">35</h1>
-            <span style={{ fontSize: "12px" }}>DAYS</span>
+            <span className="xsmall-font">DAYS</span>
           </div>
-          <div className="col-3" style={{ borderRight: "1px solid" }}>
+          <div
+            className="col-3 center-content"
+            style={{ borderRight: "1px solid" }}
+          >
             <h1 className="small-font">07</h1>
-            <span style={{ fontSize: "12px" }}>HOURS</span>
+            <span className="xsmall-font">HOURS</span>
           </div>
 
-          <div className="col-3" style={{ borderRight: "1px solid" }}>
+          <div
+            className="col-3 center-content"
+            style={{ borderRight: "1px solid" }}
+          >
             <h1 className="small-font">23</h1>
-            <span style={{ fontSize: "12px" }}>MINUTES</span>
+            <span className="xsmall-font">MINUTES</span>
           </div>
 
-          <div className="col-3">
+          <div className="col-3 center-content">
             <h1 className="small-font">09</h1>
-            <span style={{ fontSize: "12px" }}>SECONDS</span>
+            <span className="xsmall-font">SECONDS</span>
           </div>
         </div>
       </div>
