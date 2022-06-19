9bbd26decf874169177b1c4eebf23d7cc50e4dd6
hatem <hatemazaiez1@gmail.com>
2022-06-19 21:57:55 +0100

    test

--- src/components/CardTransactionCapsule2.js
+++ src/components/CardTransactionCapsule2.js
@@ -3,9 +3,10 @@ import "./CardTransactionCapsule.css";
 import ExchangeModal from "./modals/ExchangeModal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3 from "web3";
+import {  } from "@celo/contractkit";
 import { abiDepositContract } from "./abiDepositContract";
 import { ERC20abi } from "./ERC20abi";
-import { requestTxSig, waitForSignedTxs,requestAccountAddress, waitForAccountAuth ,FeeCurrency , newKitFromWeb3,toTxResult} from "@celo/contractkit";
+import { requestTxSig, waitForSignedTxs,requestAccountAddress, waitForAccountAuth ,FeeCurrency , newKitFromWeb3,toTxResult} from "@celo/dappkit";
 
 import { useHistory, useParams } from "react-router-dom";
 import { ethers } from "ethers";
