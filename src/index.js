f93162ecce69510d5b5955687af5fae3552a62d0
hatem <hatemazaiez1@gmail.com>
2022-06-20 00:36:25 +0100

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CeloProvider,useCelo,SupportedProvider } from '@celo/react-celo';
+import { CeloProvider,useCelo, Alfajores, NetworkNames } from '@celo/react-celo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
