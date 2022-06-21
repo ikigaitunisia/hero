2e7f3cd38a7e50c53a17555a421b5369fe2162ad
takwa <takouaguetat@gmail.com>
2022-06-21 14:50:34 +0100

    remove bottomMenu when not logged in from feed page

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import FeedCapsule from "../components/FeedCapsule";
 import Header from "../components/Header";
 import BottomMenu from "../components/BottomMenu";
 
 function Feed(props) {
+  const [loggedin, setLogedin] = useState(false);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user != null) {
+      setLogedin(true);
+    }
+  }, []);
   return (
     <>
-      <Header showLogo showMenuBtn showLoginBtn/>
+      <Header showLogo showMenuBtn showLoginBtn />
       <FeedCapsule />
-      <BottomMenu showFeedBtn showCardBtn showVoteBtn activeItem="feed" />
+      {loggedin && (
+        <BottomMenu showFeedBtn showCardBtn showVoteBtn activeItem="feed" />
+      )}
     </>
   );
 }
