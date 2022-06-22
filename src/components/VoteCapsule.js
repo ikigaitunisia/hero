e0888f0f085ff436f2dda5394b33cda35a916d58
takwa <takouaguetat@gmail.com>
2022-06-22 17:03:35 +0100

    fix vote page

--- src/components/VoteCapsule.js
+++ src/components/VoteCapsule.js
@@ -1,73 +1,66 @@
-import React,{useEffect,useReducer,useState} from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import "./VoteCapsule.css";
-import axios from 'axios';
+import axios from "axios";
 function VoteCapsule(props) {
-  const [proposal,setProposal] = useState([]);
-  const [nbVotes,setnbVotes] = useState(0);
-  const [yesPercent,setyesPercent] = useState(0);
-  const [noPercent,setnoPercent] = useState(0);
-  const [voted,setVoted] = useState(false);
-  const [priv, setPriv] =  useState("");
-  const voteOnproposal = (_id,vote) => {
-
+  const [proposal, setProposal] = useState([]);
+  const [nbVotes, setnbVotes] = useState(0);
+  const [yesPercent, setyesPercent] = useState(0);
+  const [noPercent, setnoPercent] = useState(0);
+  const [voted, setVoted] = useState(false);
+  const [priv, setPriv] = useState("");
+  const voteOnproposal = (_id, vote) => {
     axios
-    .post("https://hegemony.donftify.digital:8080/VoteProp", 
-      {
-        "privKey" : priv,
-        "id" : _id,
-        "vote" : vote
-    
-    }
-    )
-    .then(function (response) {
-      console.log(response.data);
-
-      setVoted(true);
-    })
-    .catch(function (error) {
-        //handle error here
-        console.log(error);
-    }); 
-  }
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    console.log(user);
-    setPriv (user.wallet.autre.privateKey);
-    axios
-    .post("https://hegemony.donftify.digital:8080/GetIndexProp")
-    .then(function (response) {
-      console.log(response.data);
-      setnbVotes(parseInt(response.data));
-
-      for (var i=1;i < response.data ;i++)
-      {
-        console.log("ok");
-      axios
-      .post("https://hegemony.donftify.digital:8080/GetProposal", {
-        id: i
+      .post("https://hegemony.donftify.digital:8080/VoteProp", {
+        privKey: priv,
+        id: _id,
+        vote: vote,
       })
       .then(function (response) {
         console.log(response.data);
-        
-        setProposal([...proposal,response.data]);
+
+        setVoted(true);
       })
       .catch(function (error) {
-          //handle error here
-          console.log(error);
-      }); 
-      }
-    })
-    .catch(function (error) {
         //handle error here
         console.log(error);
-    }); 
-  
+      });
+  };
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
+    setPriv(user.wallet.autre.privateKey);
+    axios
+      .post("https://hegemony.donftify.digital:8080/GetIndexProp")
+      .then(function (response) {
+        console.log(response.data);
+        setnbVotes(parseInt(response.data));
 
+        for (var i = 1; i < response.data; i++) {
+          console.log("ok");
+          axios
+            .post("https://hegemony.donftify.digital:8080/GetProposal", {
+              id: i,
+            })
+            .then(function (response) {
+              console.log(response.data);
+
+              setProposal([...proposal, response.data]);
+            })
+            .catch(function (error) {
+              //handle error here
+              console.log(error);
+            });
+        }
+      })
+      .catch(function (error) {
+        //handle error here
+        console.log(error);
+      });
   }, []);
   return (
     <div id="appCapsule" className="bg-g" style={{ minHeight: "100vh" }}>
       <div className="section custom-width center-div-2 white-text mb-4 mt-4">
-        <h2 className="white-text text-title">
+        <h2 className="white-text text-title" style={{ fontWeight: "700" }}>
           You have the power to vote on the future of HERO.
         </h2>
         <p>
@@ -83,80 +76,10 @@ function VoteCapsule(props) {
           Discover how voting works.
         </a>
       </div>
-
-      { proposal.map((prop,i) => (
-      <>
       <div className="section center-div-2 mb-4">
-        <div className="wallet-card">
-          <p style={{ color: "black", fontWeight: "bold" }}>
-            {prop.Description}
-          </p>
-        </div>
-      </div>
-      <div className="section mb-4 center-div-2">
-        <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 flex-center mb-2">
-            <button
-              type="button"
-              className="btn btn-icon rounded me-1"
-              style={{
-                backgroundColor: "black",
-                width: "50px",
-                height: "50px",
-              }}
-              onClick = {()=>voteOnproposal(i+1,true)}
-
-            >
-              <ion-icon
-                name="close"
-                style={{ color: "white", display: "flex" }}
-              ></ion-icon>
-            </button>
-            {voted &&
-            <div>
-              <span
-                className="badge badge-dark custom-badge"
-                style={{ borderRadius: "30px" }}
-              >
-                20%
-              </span>
-            </div>
-            }
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 flex-center mb-2">
-            <button
-              type="button"
-              className="btn btn-icon rounded me-1"
-              style={{
-                backgroundColor: "white",
-                width: "50px",
-                height: "50px",
-              }}
-              onClick = {()=>voteOnproposal(i+1,true)}
-            >
-              <ion-icon
-                name="checkmark-outline"
-                style={{ color: "black", display: "flex" }}
-              ></ion-icon>
-            </button>
-            {voted &&
-            <div>
-              <span
-                className="badge badge-dark custom-badge"
-                style={{ borderRadius: "30px" }}
-              >
-                80%
-              </span>
-            </div>
-            }
-           
-          </div>
-        </div>
-      </div>
-      <div className="section center-div-2 mb-4 pb-4">
-        <h3 className="white-text" style={{ textDecoration: "underline" }}>
+        <h5 className="white-text mb-2" style={{ textDecoration: "underline" }}>
           Voting ends in:
-        </h3>
+        </h5>
         <div className="row wallet-card custom-padding mt-3">
           <div
             className="col-3 center-content"
@@ -187,10 +110,76 @@ function VoteCapsule(props) {
           </div>
         </div>
       </div>
-      </>
+      {proposal.map((prop, i) => (
+        <>
+          <div className="section center-div-2 mb-4" style={{ padding: 0 }}>
+            <div className="wallet-card">
+              <p style={{ color: "black", fontWeight: "bold" }}>
+                {prop.Description}
+              </p>
+            </div>
+          </div>
+          <div className="section mb-4 center-div-2">
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 flex-center mb-2">
+                <button
+                  type="button"
+                  className="btn btn-icon rounded me-1"
+                  style={{
+                    backgroundColor: "black",
+                    width: "50px",
+                    height: "50px",
+                  }}
+                  onClick={() => voteOnproposal(i + 1, true)}
+                >
+                  <ion-icon
+                    name="close"
+                    style={{ color: "white", display: "flex" }}
+                  ></ion-icon>
+                </button>
+                {voted && (
+                  <div>
+                    <span
+                      className="badge badge-dark custom-badge"
+                      style={{ borderRadius: "30px" }}
+                    >
+                      20%
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12 flex-center mb-2">
+                <button
+                  type="button"
+                  className="btn btn-icon rounded me-1"
+                  style={{
+                    backgroundColor: "white",
+                    width: "50px",
+                    height: "50px",
+                  }}
+                  onClick={() => voteOnproposal(i + 1, true)}
+                >
+                  <ion-icon
+                    name="checkmark-outline"
+                    style={{ color: "black", display: "flex" }}
+                  ></ion-icon>
+                </button>
+                {voted && (
+                  <div>
+                    <span
+                      className="badge badge-dark custom-badge"
+                      style={{ borderRadius: "30px" }}
+                    >
+                      80%
+                    </span>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </>
       ))}
     </div>
-    
   );
 }
 
