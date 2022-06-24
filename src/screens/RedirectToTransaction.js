ea45021c919aced7c08815c67bf351ed011fa821
hatem <hatemazaiez1@gmail.com>
2022-06-24 21:16:14 +0100

    Envoi

--- src/screens/RedirectToTransaction.js
+++ src/screens/RedirectToTransaction.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
+import React, { PropTypes, Component } from 'react';
 import { withRouter } from "react-router-dom";
 
 
 
+class RedirectToTransaction extends Component {
+    onUnload = e => { // the method that will be used for both add and remove event
+       e.preventDefault();
+       window.open("https://hegemony.donftify.digital:3001/cardtransaction", '_blank', 'noopener,noreferrer');
+      }
 
-function RedirectToTransaction(props) {
-  useEffect(() => {
-    window.addEventListener("beforeunload", (ev) => 
-{  
-    ev.preventDefault();
-    window.open("https://hegemony.donftify.digital:3001/cardtransaction", '_blank', 'noopener,noreferrer');
-  });
-  }, []);
-  return (
-    <>
-     
-    </>
-  );
+    componentDidMount() {
+       window.addEventListener("beforeunload", this.onUnload);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("beforeunload", this.onUnload);
+    }
+
+    render() {
+        return (
+            <div>
+                Some content
+            </div>
+        );
+    }
+
 }
 
-export default withRouter(RedirectToTransaction);
+export default MyComponent
