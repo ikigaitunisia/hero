6cc9ec734db0cdba34ab4f6b11dedad7de224b80
takwa <takouaguetat@gmail.com>
2022-06-24 18:26:36 +0100

    remove change password

--- src/components/modals/TransactionDigitalCurrencyModal.js
+++ src/components/modals/TransactionDigitalCurrencyModal.js
@@ -52,14 +52,18 @@ function TransactionDigitalCurrencyModal(props) {
                 </p>
                 <ul className="white-text pb-2">
                   <li className="no-padding">
-                    Click a <b>first</b> time on “Validate” to proceed to Valora
+                    Click a <b>first</b> time on “Confirm” to proceed to Valora
                     Alfajoles and Approve in Valora the connection with Hero
                     Coin
                   </li>
                   <li className="no-padding">
-                    Return to this same Transaction page and Click a second time
-                    on <b>“Validate”</b> to proceed again to Valora and validate
-                    the transaction this time
+                    Return to this same Transaction page, wait a few seconds for
+                    the first operation to clear and Click a second time on{" "}
+                    <b>“Confirm”</b> to proceed again to Valora and validate the
+                    transaction this time
+                  </li>
+                  <li className="no-padding">
+                    On iphones, you even need to do this 3 times:)
                   </li>
                 </ul>
                 <p className="pb-2">
@@ -67,21 +71,25 @@ function TransactionDigitalCurrencyModal(props) {
                     This is only for the testing envirnonment, it only requires
                     1 Validation on the Real Net !
                   </b>
-                </p><br/>
+                </p>
+                <br />
                 <button
-              type="button"
-              class="btn btn-link rounded btn-lg"
-              data-bs-dismiss="modal"
-              style={{
-                borderColor: "white",
-                color: "#8585FF",
-                width: "200px",
-                backgroundColor: "white",
-              }}
-              onClick={() =>  {props.connectWallet();props.onClose()}} 
-            >
-              Confirm
-            </button>
+                  type="button"
+                  class="btn btn-link rounded btn-lg"
+                  data-bs-dismiss="modal"
+                  style={{
+                    borderColor: "white",
+                    color: "#8585FF",
+                    width: "200px",
+                    backgroundColor: "white",
+                  }}
+                  onClick={() => {
+                    props.connectWallet();
+                    props.onClose();
+                  }}
+                >
+                  Confirm
+                </button>
               </div>
             </div>
           </div>
