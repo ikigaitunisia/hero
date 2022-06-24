3f9786852fa1704e12214a1f2621989d44d01f62
hatem <hatemazaiez1@gmail.com>
2022-06-24 18:37:11 +0100

    Browser

---
+++ src/screens/Redirect.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+useEffect(() => {
+  browser.tabs.get(info.tabId);
+}, []);
+
+function Redirect(props) {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default withRouter(Redirect);
