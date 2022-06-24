ddc40c03d37df6532ba9c0b87c7357ac4feeecfa
takwa <takouaguetat@gmail.com>
2022-06-24 18:48:44 +0100

    some fixes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,12 +3,15 @@ import { withRouter } from "react-router-dom";
 import Header from "../components/Header";
 import HomeCapsule from "../components/HomeCapsule";
 import BottomMenu from "../components/BottomMenu";
+import { isMobile } from "react-device-detect";
+import HomeCapsuleDesktop from "../components/desktop/HomeCapsuleDesktop";
 
 function Home(props) {
   return (
     <>
-      <Header whiteMode showLogo showMenuBtn showLoginBtn/>
-      <HomeCapsule />
+      <Header whiteMode showLogo showMenuBtn showLoginBtn />
+      {isMobile && <HomeCapsule />}
+      {!isMobile && <HomeCapsuleDesktop />}
       <BottomMenu showFeedBtn showCardBtn showVoteBtn activeItem="" />
     </>
   );
