1ff89d6ce15d9f1f7ecba65324d8b3179e2434ba
hatem <hatemazaiez1@gmail.com>
2022-06-24 20:56:16 +0100

    useeffect

--- src/screens/RedirectToTransaction.js
+++ src/screens/RedirectToTransaction.js
@@ -2,11 +2,12 @@ import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 
 
-useEffect(() => {
-  window.open("https://hegemony.donftify.digital:3001/cardtransaction", '_blank', 'noopener,noreferrer');
-}, []);
+
 
 function RedirectToTransaction(props) {
+  useEffect(() => {
+    window.open("https://hegemony.donftify.digital:3001/cardtransaction", '_blank', 'noopener,noreferrer');
+  }, []);
   return (
     <>
      
