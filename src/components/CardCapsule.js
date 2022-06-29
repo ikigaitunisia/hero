bb538126c3718d798ef75c441963a15061efa8f0
takwa <takouaguetat@gmail.com>
2022-06-29 11:40:50 +0100

    fix card

--- src/components/CardCapsule.js
+++ src/components/CardCapsule.js
@@ -34,44 +34,44 @@ function CardCapsule(props) {
     }
     fetchBalance();
 
-    let OrderID = (new URLSearchParams(window.location.search)).get("orderId")
-    let fiatAmount = (new URLSearchParams(window.location.search)).get("fiatAmount")
-    let arrA = (new URLSearchParams(window.location.search)).get("ArrAct")
-    let ArrAmnt = (new URLSearchParams(window.location.search)).get("ArrAmnt")
+    let OrderID = new URLSearchParams(window.location.search).get("orderId");
+    let fiatAmount = new URLSearchParams(window.location.search).get(
+      "fiatAmount"
+    );
+    let arrA = new URLSearchParams(window.location.search).get("ArrAct");
+    let ArrAmnt = new URLSearchParams(window.location.search).get("ArrAmnt");
     const user = JSON.parse(localStorage.getItem("user"));
-      const wallet = user.wallet.address;
-    console.log(OrderID,fiatAmount);
+    const wallet = user.wallet.address;
+    console.log(OrderID, fiatAmount);
     console.log(arrA);
     console.log(ArrAmnt);
-  if (OrderID != undefined)
-  { 
+    if (OrderID != undefined) {
+      axios
+        .post("https://hegemony.donftify.digital:8080/DepositCusdCredit", {
+          contributer: wallet,
+          amount: parseInt(fiatAmount).toString(),
+          array: arrA.toString(),
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          //handle error here
+          console.log(error);
+        });
+    }
     axios
-    .post("https://hegemony.donftify.digital:8080/DepositCusdCredit", {
-      contributer: wallet,
-      amount: (parseInt(fiatAmount)).toString(),
-      array:arrA.toString()
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      //handle error here
-      console.log(error);
-    });
-  }
-  axios
-  .post("https://hegemony.donftify.digital:8080/HistoryTransactions", {
-    User: wallet,
-    
-  })
-  .then(function (response) {
-    console.log(response.data);
-    setTxs(response.data);
-  })
-  .catch(function (error) {
-    //handle error here
-    console.log(error);
-  }); 
+      .post("https://hegemony.donftify.digital:8080/HistoryTransactions", {
+        User: wallet,
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setTxs(response.data);
+      })
+      .catch(function (error) {
+        //handle error here
+        console.log(error);
+      });
   }, []);
   return (
     <div id="appCapsule" className="bg-g-1 cardCapsule">
@@ -84,7 +84,7 @@ function CardCapsule(props) {
                 <strong>
                   <ion-icon
                     src="assets/img/svg/Vector.svg"
-                    style={{ marginRight: "5px" }}
+                    class="me-1"
                   ></ion-icon>{" "}
                   {balance == 0 ? 1 : balance}
                 </strong>
@@ -103,10 +103,7 @@ function CardCapsule(props) {
         onClose={() => setShowExchangeModal(false)}
       />
       <div className="text pt-4">
-        <div
-          className="lead blue-text title-font custom-font-size"
-          style={{ fontWeight: "normal" }}
-        >
+        <div className="lead blue-text title-font custom-font-size fw-normal">
           You recieve 1 HERO Coin for being a first time Contributor. Then, each
           time your contribution is withdrawn by the mobilizer(s) you have
           chosen to support, you receive the equivalent amount of HERO Coins.
@@ -117,8 +114,7 @@ function CardCapsule(props) {
         <span className="pt-4">
           <a
             href="https://herolabsco.notion.site/Discover-carbon-credits-are-Calculated-7069545cfff94df7b1fd2a3831a49f2f"
-            className=" blue-text title-font link-size"
-            style={{ textDecoration: "underline" }}
+            className=" blue-text title-font link-size underline"
             target="_blank"
           >
             *Discover how carbon credits are calculated.
@@ -126,51 +122,43 @@ function CardCapsule(props) {
         </span>
         <h1 className="text-title title-font">HERO Mobilizers supported</h1>
       </div>
-      <hr
-        data-content="AND"
-        className="hr-text mt-4 mb-4"
-        style={{ width: "70%", margin: "0 auto", background: "blue" }}
-      ></hr>
       <div className="center-div">
+        <hr data-content="AND" className="hr-text mt-4 mb-4 blue-divider"></hr>
         <ul className="listview flush transparent simple-listview no-space mt-3">
-        {Txs.map((activist) => (
-
-          <li className="pt-3 pb-3">
-            <div
-              className="detail"
-              style={{ display: "flex", alignItems: "center" }}
-            >
-              <img
-                src={"assets/img/"+activist.autre.Photo}
-                alt="img"
-                className="image-block imaged rounded w48"
-                style={{ marginRight: "10px" }}
-              />
-              <div>
-                <strong className="blue-text custom-font">
-                  {activist.Nom+" "+activist.Prenom+" "+activist.contribution+"€"}
-                </strong>
+          {Txs.map((activist) => (
+            <li className="pt-3 pb-3">
+              <div className="detail flex-center">
+                <img
+                  src={"assets/img/" + activist.autre.Photo}
+                  alt="img"
+                  className="image-block imaged rounded w48"
+                  style={{ marginRight: "10px" }}
+                />
+                <div>
+                  <strong className="blue-text custom-font">
+                    {activist.Nom +
+                      " " +
+                      activist.Prenom +
+                      " " +
+                      activist.contribution +
+                      "€"}
+                  </strong>
+                </div>
               </div>
-            </div>
-            <div className="itemList">
-              <ion-icon
-                class="md-icon"
-                src="assets/img/svg/voteBlue.svg"
-                style={{ marginRight: "5px" }}
-                onClick={() => setShowMobilizerCampaignsModal(true)}
-              ></ion-icon>
-              <ion-icon
-                class="md-icon"
-                src="assets/img/svg/HeroCoin2.svg"
-                style={{ marginRight: "5px" }}
-                onClick={() => setShowMobilizerVictoriesModal(true)}
-              ></ion-icon>
-            </div>
-          
-          
-          </li>
-       )) }
-          
+              <div className="itemList">
+                <ion-icon
+                  class="md-icon me-1"
+                  src="assets/img/svg/voteBlue.svg"
+                  onClick={() => setShowMobilizerCampaignsModal(true)}
+                ></ion-icon>
+                <ion-icon
+                  class="md-icon me-1"
+                  src="assets/img/svg/HeroCoin2.svg"
+                  onClick={() => setShowMobilizerVictoriesModal(true)}
+                ></ion-icon>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
       {/*<ActivistCampaignsModal
