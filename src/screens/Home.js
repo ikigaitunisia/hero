0dd92cd8fedbcc94a7ed483c71e3b512ef373064
takwa <takouaguetat@gmail.com>
2022-07-18 11:23:25 +0100

    add home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,7 +3,36 @@ import { withRouter } from "react-router-dom";
 import "./Home.css";
 
 function Home(props) {
-  return <></>;
+  const join = () => {};
+  return (
+    <>
+      <div class="appHeader no-border transparent position-absolute">
+        <div class="left"></div>
+        <div class="pageTitle"></div>
+        <div class="right"></div>
+      </div>
+
+      <div id="appCapsule" className="home">
+        <img
+          src={"assets/img/HEROLogoBlue.png"}
+          alt="logo"
+          className="logo home-logo"
+        />
+        <p className="m-4 home-text">
+          Welcome to the first global
+          <br /> community where supporters and
+          <br /> mobilizers unite for climate action.
+        </p>
+        <button
+          type="button"
+          className="btn btn-outline-secondary btn-lg rounded"
+          onClick={join}
+        >
+          TAP TO JOIN
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default withRouter(Home);
