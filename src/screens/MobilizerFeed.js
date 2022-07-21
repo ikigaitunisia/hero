bad803c8b29986286332e6299504791abe19aec3
hatem <hatemazaiez1@gmail.com>
2022-07-21 14:55:51 +0100

    fix

--- src/screens/MobilizerFeed.js
+++ src/screens/MobilizerFeed.js
@@ -3,7 +3,7 @@ import { withRouter, useHistory } from "react-router-dom";
 import "./MobilizerFeed.css";
 import InfiniteScroll from "react-infinite-scroll-component";
 import EchoModal from "../components/EchoModal";
-import MobilizerData from "../data/data.js";
+import {MobilizerData} from "../data/data.js";
 const SCROLL_UP = "up";
 const SCROLL_DOWN = "down";
 
