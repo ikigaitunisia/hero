d77e61b8c9ed5e68c6091f41894810c3fafa5384
hatem <hatemazaiez1@gmail.com>
2022-07-21 14:10:07 +0100

    json

---
+++ src/data/data.js
@@ -0,0 +1,208 @@
+export const MobilizerData = [
+    {
+        // re form this pls
+        id: "Elijah",
+        name: "Elijah McKenzie",
+        profileDescription: "Co-Founder and Co-Executive at Waic Up | Environmental Advocate and Artist",
+        Email :"juanpablosierrasuarez@gmail.com",
+        LocationHighlight: "UK",
+        location: "London, UK",
+        phone: "57 (300) 7189525",
+        sm: "IG: @elijahmckenziejackson",
+        mainMovements: "Fridays for Future UK",
+        video: "elijahVideo.mp4",
+        TopCampaigns:
+        [
+            {title: "No more fast fashion",
+            campaign: "Fridays for Future UK",
+            status: "In progress",
+            desc: "campaigning to boycott brands with fast fashion business models, characterised by quick item turnover,\
+            low prices, exploitative working conditions for clothes manufacturers and an extremely high environmental footprint."
+        } ,
+        { 
+            title: "Fossil fuel phaseout",
+            campaign: "Independent Mobilizer",
+            desc: "Campaigning to stop Cambo and Jackdaw fossil fuel projects in the UK.\
+                    Mobilising around the Fossil Fuel Non-Proliferation Treaty.",
+            campaing: "Fridays for Future UK",
+            status: "In progress",
+        },
+        { title: "Official ambassador UN Together Band",
+            campaign: "Goals 13 and 14 on Climate Action and Life Below Water",
+            desc: "UN Together Band Official is a global movement aiming to\
+            build a worldwide community of 1 billion citizens to achieve the 17 UN Sustainable Development Goals.",
+            status: "In progress",
+
+        }
+    ],
+
+
+    Victories: [
+        {title: "Has spoken at the UK Parliament",  campaign:"the House of Lords", status: "Completed" , desc: "The EU Parliament, outside the G7 Conference, and in the Amazon rainforest about the climate crisis."},
+        {title:"Interviewed for The Nature of Change",campaign: "The Nature of Change" , status: "Completed" ,desc: "an exclusive book and fundraising campaign featuring 15 youth climate activists from 11 countries. All profits go to climate crisis charities."},
+        {title:"Featured in GQ Turkey Magazine", campaign: "Earth and sustainability edition", status: "Completed" , desc: "Featured in GQ Turkey Magazine's Earth and sustainability edition featuring oil spill and plastic pollution 2019 visual art."}
+    ],
+    
+
+
+},
+    {
+        id: "Julieta",
+        name: "Julieta Martinez",
+        profileDescription: "Founder Tremendas | Cofounder Latinas for climate | Activist UN Women",
+        Email :"julieta@tremendas.cl",
+        LocationHighlight: "Chile",
+        location: "Santiago, Chile",
+        phone: "",
+        sm: "linktr.ee/julietamartinez",
+        mainMovements: "",
+        video: "julietaVideo.mp4",
+        Victories: [
+            {
+                title:"Chilean Women Creating Future ", 
+                campaign: "Chile’s Country Brand",
+                status: "Completed",
+                desc:"Portrayed as one of Chilean women creating future for Chile and the world with their talent."
+            },
+            {
+                title:"Tedx Speaker", 
+                campaign: "TED",
+                status: "Completed",
+                desc:"Girls' education is a climate solution.In her talk, emphasis is placed on the importance and role of girls' education, considering various factors that today can have an impact on our society, along with various changes that we can adopt to reduce the gaps that exist in the world."
+              },            
+            {
+                title:"Assigned as Ambassador", 
+                campaign: "Ashoka Foundation",
+                status: "Completed",
+                desc:"Recognized as one of the world's leading social entrepreneurs raising awareness on climate change and gender equality."
+            },
+        ],
+        TopCampaigns: [
+
+            {
+                title: "Adolescent Empowerment",
+                campaign: "Tremendas Foundation",
+                status: "In progress",
+                desc: "Promoting the empowerment of adolescents and young people by inviting them to put their skills and talents at the service of the community to generate social impact."
+            },
+            {title:"Youth Task Force", campaign: "UN Women",status: "In progress",desc:"Leadership and youth participation in the next Generation Equality Forum, a global meeting focused on civil society for gender equality."},
+            {title:"International Network of Young Latin American Activists", campaign: "Latinas for Climate",status: "In progress",desc:"Latinas for Climate seeks to raise awareness about the climate crisis with a gender, Human Rights and Latin perspective, in order to educate girls, young people and women in the region on this issue so that they become empowered and begin to take action for a better future."  }
+        ]
+    },
+    {
+        // For Jessica:  for all campaigns
+        id: "Jessica",
+        name: "Jessica Kleczka",
+        profileDescription: "Climate Psychology and Communication Researcher | Independent Advocate and Writer",
+        Email :"hello@jessicakleczka.com",
+        LocationHighlight: "UK",
+        location: "Aberystwyth, Wales; and London, UK",
+        phone: "",
+        sm: "IG: @jessicakleczka",
+        mainMovements: "Indepedent mobiliser",
+        video: "jessicaVideo.mp4",
+        Victories: [
+        {
+            title: "TEDx talk ",
+            campaign: "TEDx Aberystwyth",
+            status: "Completed",
+            desc: "Dismantling the way we think about climate change. Spoke about the public's perception of the climate crisis and what factors influence how people think and talk about it."
+        },            
+        {
+            title: "Interviewed by activists",
+            campaign: "Stop Cambo oil field interview series",
+            status: "Completed",
+            desc: "Spoke to Ya'ara Peretz about energy security, new models of extraction, and how the proposed EastMed pipeline is fueling geopolitical conflicts."
+        },
+        {
+            title:"Interviewed on the Story Anew podcast",
+            campaign:"Shifting public consciousness about climate change.",
+            status:"Completed",
+            desc: "Spoke about why it is important that climate policy reflects human psychology, the role of the media in shifting our values, and the challenges of fighting for systemic change."
+        }
+    ],
+        TopCampaigns: [
+                {
+                    title: "Fossil fuel phaseout",
+                    campaign: "Independent Mobilizer",
+                    status: "In progress",
+                    desc: "Spokesperson for the Stop Cambo oil field and the Stop Jackdaw gas field campaigns in the UK. Campaigning for Stop the East African Crude Oil Pipeline (EACOP) in Tanzania and Uganda. Sharing the Fossil Fuel Non-Proliferation Treaty."
+                }, 
+                {
+                    title: "Cost of living crisis in the UK",
+                    campaign: "Independent Mobilizer",
+                    status: "In progress",
+                    desc: "Worked on an explainer video with the COP 26 coalition to draw attention to the issue of energy poverty and available solutions. Campaigning for an affordable price cap on energy bills and ambitious investments in home insulation in the UK to address the issue."
+                }, 
+                {
+                    title : "Mental health and the climate crisis",
+                    campaign: "Independent Mobilizer",
+                    status: "In progress",
+                    desc: "Shares very popular positive climate news every week on her Instagram account, emphasising the importance of mental health, psychology and communication in addressing the climate crisis."
+                }
+        ],
+    },
+
+
+
+
+
+
+
+
+
+    
+    {
+        id: "Juan",
+        name: "Juan Sierra",
+        profileDescription: "Co-Director, Pacto X El Clima | Young Climate Expert at Building Bridges for Climate Action",
+        Email :"juanpablosierrasuarez@gmail.com",
+        LocationHighlight: "Colombia",
+        location: "Bogotá, Colombia",
+        phone: "57 (300) 7189525",
+        sm: "IG: @juansierra99, @juanpasierra11",
+        mainMovements: "Pacto X El Clima Colombia Fridays For Future Bogotá United for Climate Action",
+        video: "juanVideo.mp4",
+        Victories: [
+            {
+                title: "Pacto X El Clima Colombia",
+                campaing: "Climate Adaptation Conference",
+                status: "Completed",
+                desc: "Guest speaker at the Youth for Climate Adaptation Conference. Speech focused on the importance of climate adaptation, not only mitigation, with a focus on vulnerable communities in order to build a just and resilient world."
+            },
+
+            {title: "Fridays For Future Bogotá",
+             campaign:"Future Bogotá",
+             status: "Completed",
+             desc: "Widespread media coverage with Fridays for Future Bogotá across TV, newspaper and radio channels in Colombia raising awareness on a variety of climate issues."
+            },
+             {
+                title: "United for Climate Action",
+                campaign:"COP 26",
+                status: "Completed",
+                desc: "Represented Colombian and Latin American civil society voices at COP 26. Spoke to the President of Colombia about the country's shortcomings regarding climate action and the environmental leaders that continue to be persecuted for their work."}
+            ],
+        TopCampaigns: [
+            {
+                title: "Pacto X",
+                campaign: "El Clima Colombia",
+                status: "In progress",                
+                desc: "Co-Director at Pacto X El Clima Colombia: Pacto's mission is to transform Latin American society with youth to build a more prosperous, equitable and peaceful world in harmony with the ecological environment through communication, education, art, research, and actions."
+            }, 
+            {
+                title: "Fridays For Future Bogotá",
+                campaign: "Future Bogotá",
+                status: "In progress",
+                desc: "FFF is a youth-led and organised international climate strike movement of students walking out of school on Fridays, started by Greta Thunberg."
+            }, 
+            {
+                title: "United",
+                campaign: "Climate Action",
+                status: "In progress",
+                desc: "U4CA is a diverse group of young people working toward greater accessibility and intersectioinality in international environmental decision-making spheres. U4CA aims to shift the balance of power primarily toward representation of regions seriously affected by the climate crisis by 2025."
+            }
+        ],
+    },
+
+
+]
--- src/screens/MobilizerFeed.js
+++ src/screens/MobilizerFeed.js
@@ -3,14 +3,62 @@ import { withRouter, useHistory } from "react-router-dom";
 import "./MobilizerFeed.css";
 import InfiniteScroll from "react-infinite-scroll-component";
 import EchoModal from "../components/EchoModal";
+const SCROLL_UP = "up";
+const SCROLL_DOWN = "down";
 
+const useScrollDirection = ({
+  initialDirection,
+  thresholdPixels,
+  off
+} = {}) => {
+  const [scrollDir, setScrollDir] = useState(initialDirection);
+
+  useEffect(() => {
+    const threshold = thresholdPixels || 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        // We haven't exceeded the threshold
+        ticking = false;
+        return;
+      }
+
+      setScrollDir(scrollY > lastScrollY ? SCROLL_DOWN : SCROLL_UP);
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    /**
+     * Bind the scroll handler if `off` is set to false.
+     * If `off` is set to true reset the scroll direction.
+     */
+    !off
+      ? window.addEventListener("scroll", onScroll)
+      : setScrollDir(initialDirection);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [initialDirection, thresholdPixels, off]);
+
+  return scrollDir;
+};
 function MobilizerFeed(props) {
   const history = useHistory();
 
   const [items, setItems] = useState(Array.from({ length: 2 }));
   const [showEchoModal, setShowEchoModal] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-
+  
   const fetchMoreData = () => {
     if (items.length >= 6) {
       setHasMore(false);
