0b8d948fa0285d7c4507b27a8ed257442956d719
takwa <takouaguetat@gmail.com>
2022-08-08 11:01:35 +0100

    first commit

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import Header from "../components/Header";
-import HomeCapsule from "../components/HomeCapsule";
 import BottomMenu from "../components/BottomMenu";
-import { isMobile } from "react-device-detect";
-import HomeCapsuleDesktop from "../components/desktop/HomeCapsuleDesktop";
 
 function Home(props) {
   return (
     <>
       <Header whiteMode showLogo showMenuBtn showLoginBtn />
-      {isMobile && <HomeCapsule />}
-      {!isMobile && <HomeCapsuleDesktop />}
       <BottomMenu showFeedBtn showCardBtn showVoteBtn activeItem="" />
     </>
   );
