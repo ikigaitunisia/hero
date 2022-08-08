09c11c401bf010e0b3b6a7ce8c836020b39e94e0
takwa <takouaguetat@gmail.com>
2022-08-08 17:16:02 +0100

    fix last commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import "./components/common.css"
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 import ChangePassword from "./screens/ChangePassword";
+import SearchFilters from "./screens/SearchFilters";
+import Feed from "./screens/Feed";
 function App() {
   return (
     <div className="App">
       <Router>
         <Route path="/login" exact component={Login} />
         <Route path="/" exact component={Home} />
+        <Route path="/search-filters" exact component={SearchFilters} />
+        <Route path="/feed" exact component={Feed} />
         <Route path="/changepassword" exact component={ChangePassword} />        
       </Router>
     </div>
---
+++ src/screens/Feed.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import Header from "../components/Header";
+import SearchForHeroCircle from "../components/SearchForHeroCircle";
+import "./Home.css";
+
+function Feed(props) {
+  return (
+    <>
+      <Header whiteMode showLogo showMenuBtn showLoginBtn />
+
+      <div id="appCapsule" class="home-container">
+        <SearchForHeroCircle />
+      </div>
+    </>
+  );
+}
+
+export default withRouter(Feed);
