415c9b81ed8ab85e5b4ab0315d7fb89f23d9a55c
takwa <takouaguetat@gmail.com>
2022-09-05 17:30:59 +0100

    fix last commit

--- src/screens/CircleUpdates.js
+++ src/screens/CircleUpdates.js
@@ -21,15 +21,15 @@ function CircleUpdates(props) {
       <div id="appCapsule" className="circles-updates">
         <div className="mt-4">
           <ion-icon
+            src="assets/img/svg/icon18.svg"
+            class="mb-2 icon-btn me-2"
+          ></ion-icon>
+          <ion-icon
             src="assets/img/svg/icon16.svg"
             class="mb-2 icon-btn me-2"
           ></ion-icon>
           <ion-icon
             src="assets/img/svg/icon17.svg"
-            class="mb-2 icon-btn me-2"
-          ></ion-icon>
-          <ion-icon
-            src="assets/img/svg/icon18.svg"
             class="mb-2 icon-btn me-2"
           ></ion-icon>
         </div>
