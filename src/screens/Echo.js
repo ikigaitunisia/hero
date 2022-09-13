1c1fe6d4d92283d6ec77af49894510c9e7fdfa9c
takwa <takouaguetat@gmail.com>
2022-09-13 12:49:34 +0100

    fix echo page

--- src/screens/Echo.js
+++ src/screens/Echo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import "./Echo.css";
 
 function Echo() {
   const history = useHistory();
@@ -9,11 +10,11 @@ function Echo() {
     <>
       <div
         id="appCapsule"
-        className="bg-g login"
+        className="echo"
         style={{ minHeight: "100vh" }}
       >
-        <div id="welcomeClub" className="modal-body">
-          <img src={"assets/img/echo.png"} alt="logo" className="logo" />{" "}
+        <div id="" className="echo-content section">
+          <img src={"assets/img/echotransparent.png"} alt="logo" className="logo" />{" "}
           <p className="header-text mt-4 white">
             Echo this circle,
             <br />
@@ -23,7 +24,7 @@ function Echo() {
             <input
               type="text"
               class="form-control"
-              placeholder="circle1.hero-labs.co"
+              value="circle1.hero-labs.co"
               aria-label="circle1.hero-labs.co"
               aria-describedby="basic-addon2"
             />
@@ -40,7 +41,7 @@ function Echo() {
                   type="button"
                   className="btn btn-icon rounded btn-primary social-btn"
                 >
-                  <ion-icon class="blue" name="logo-whatsapp"></ion-icon>
+                  <img class="blue" src="assets/img/svg/whatsapp.svg"/>
                 </button>
               </div>
               <div className="flex-col-center col mb-3 p-0">
@@ -48,7 +49,7 @@ function Echo() {
                   type="button"
                   className="btn btn-icon rounded btn-primary social-btn"
                 >
-                  <ion-icon src="assets/img/svg/tiktok2.svg"></ion-icon>
+                  <img class="blue" src="assets/img/svg/tiktok3.svg"/>
                 </button>
               </div>
               <div className="flex-col-center col mb-3 p-0">
@@ -56,7 +57,7 @@ function Echo() {
                   type="button"
                   className="btn btn-icon rounded btn-primary social-btn"
                 >
-                  <ion-icon class="blue" name="logo-instagram"></ion-icon>
+                  <img class="blue" src="assets/img/svg/instagram.svg"/>
                 </button>
               </div>
               <div className="flex-col-center col mb-3 p-0">
@@ -64,7 +65,7 @@ function Echo() {
                   type="button"
                   className="btn btn-icon rounded btn-primary social-btn"
                 >
-                  <ion-icon class="blue" name="logo-twitter"></ion-icon>
+                  <img class="blue" src="assets/img/svg/twitter.svg"/>
                 </button>
               </div>
               <div className="flex-col-center col mb-3 p-0">
@@ -72,7 +73,7 @@ function Echo() {
                   type="button"
                   className="btn btn-icon rounded btn-primary social-btn"
                 >
-                  <ion-icon class="blue" name="logo-facebook"></ion-icon>
+                  <img class="blue" src="assets/img/svg/facebook.svg"/>
                 </button>
               </div>
               <div className="flex-col-center col mb-3 p-0">
@@ -80,7 +81,7 @@ function Echo() {
                   type="button"
                   className="btn btn-icon rounded btn-primary social-btn"
                 >
-                  <ion-icon class="blue" name="logo-linkedin"></ion-icon>
+                  <img class="blue" src="assets/img/svg/linkedin.svg"/>
                 </button>
               </div>
             </div>
