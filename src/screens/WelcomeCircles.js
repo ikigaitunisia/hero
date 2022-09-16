2ffdb1a60eeec6d46c843eee20f9cb1c09cc306c
takwa <takouaguetat@gmail.com>
2022-09-16 16:18:15 +0100

    fix last commit

--- src/screens/WelcomeCircles.js
+++ src/screens/WelcomeCircles.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./WelcomeCirclesModal.css";
-import { useHistory, useParams,withRouter } from "react-router-dom";
+import { useHistory, useParams, withRouter } from "react-router-dom";
 import { Modal } from "bootstrap";
 
 function WelcomeCircles(props) {
-  const history=useHistory(); 
-  const {circlename} = useParams();
+  const history = useHistory();
+  const { circlename } = useParams();
   useEffect(() => {
-      const modal = new Modal(document.getElementById("welcomeCircles"), {
-        keyboard: false,
-      });
-     
-        modal.show();
-   
+    const modal = new Modal(document.getElementById("welcomeCircles"), {
+      keyboard: false,
+    });
+
+    modal.show();
   }, []);
   useEffect(() => {
     const btn1 = document.getElementById("btn1");
     btn1.addEventListener("click", () => {
-      history.push("/echo:"+circlename.replace(':',""));
+      history.push("/echo:" + circlename.replace(":", ""));
       window.location.reload();
     });
     const btn2 = document.getElementById("btn2");
@@ -27,7 +26,7 @@ function WelcomeCircles(props) {
     });
     const btn3 = document.getElementById("btn3");
     btn3.addEventListener("click", () => {
-      history.push("/circle-home");
+      history.push("/account-information");
       window.location.reload();
     });
   }, []);
@@ -49,25 +48,24 @@ function WelcomeCircles(props) {
                 border: "unset",
                 background: "transparent",
               }}
-            >
-             
-            </div>
+            ></div>
             <div className="modal-body">
               <div className="flex-center flex-col">
                 <img src={"assets/img/logo2.png"} alt="logo" className="logo" />
                 <p className="header-text mt-4 white">
-                  Welcome to the Circle, John!
+                  Welcome to Circle 1, John!
                 </p>
                 <p className="mt-4 mb-4 pb-4 white center-text">
-                  You are now supporting{" "}
-                  <b>
-                    Mobilizer 1 and 9<br /> other mobilizers
-                  </b>{" "}
-                  and will have access to
-                  <br /> exclusive features.
+                  You are now supporting <b>the Circle 1</b> and will
+                  <br /> have access to exclusive features.
                 </p>
 
-                <button id="btn1" type="button" className="btn btn-lg mb-4" onClick={() => console.log("ok")}>
+                <button
+                  id="btn1"
+                  type="button"
+                  className="btn btn-lg mb-4"
+                  onClick={() => console.log("ok")}
+                >
                   Echo this circle,
                   <br />
                   invite your friends to join!
