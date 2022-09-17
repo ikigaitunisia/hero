fcb1312777e9010aeb1e0a89578968d965a47865
takwa <takouaguetat@gmail.com>
2022-09-17 15:46:54 +0100

    new login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import "./components/common.css"
+import "./components/common.css";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 import ChangePassword from "./screens/ChangePassword";
@@ -27,8 +27,7 @@ import AccountInformation from "./screens/AccountInformation";
 import EditPassword from "./screens/EditPassword";
 import Subscriptions from "./screens/Subscriptions";
 import WelcomeCircles from "./screens/WelcomeCircles";
-
-
+import Login1 from "./screens/Login1";
 
 function App() {
   return (
@@ -40,26 +39,55 @@ function App() {
         <Route path="/feed" exact component={Feed} />
         <Route path="/mobilizer-feed" exact component={MobilizerFeed} />
         <Route path="/mobilizer-profile" exact component={ActivistProfile} />
-        <Route path="/african-leaders-circle-profile" exact component={AfricanLeadersCircleProfile} />
-        <Route path="/changepassword" exact component={ChangePassword} />        
-        <Route path="/supporters" exact component={ListSupporter} />    
-        <Route path="/mobilizers" exact component={ListMobilizer} />   
-        <Route path="/" exact component={FindCircle} />   
-        <Route path="/circle-feed" exact component={CircleFeed} />   
-        <Route path="/circle-feed-details:circlename" exact component={CircleFeedDetails} />   
-        <Route path="/circle-home" exact component={CircleHome} />   
-        <Route path="/mobilizer-profile2:circlename" exact component={MobilizerProfile} />   
-        <Route path="/circle-updates" exact component={CircleUpdates} />   
-        <Route path="/circle-videos" exact component={CircleVideos} />   
-        <Route path="/circle-interactions" exact component={CircleInteractions} />   
-        <Route path="/CircleDashboard:circlename" exact component={CircleDashboard} /> 
-        <Route path="/hero-team" exact component={HeroTeam} />   
-        <Route path="/echo:circlename" exact component={Echo} />   
-        <Route path="/account-information" exact component={AccountInformation} />   
-        <Route path="/edit-password" exact component={EditPassword} />   
-        <Route path="/welcome-circle:circlename" exact component={WelcomeCircles}  />   
+        <Route
+          path="/african-leaders-circle-profile"
+          exact
+          component={AfricanLeadersCircleProfile}
+        />
+        <Route path="/changepassword" exact component={ChangePassword} />
+        <Route path="/supporters" exact component={ListSupporter} />
+        <Route path="/mobilizers" exact component={ListMobilizer} />
+        <Route path="/" exact component={FindCircle} />
+        <Route path="/circle-feed" exact component={CircleFeed} />
+        <Route
+          path="/circle-feed-details:circlename"
+          exact
+          component={CircleFeedDetails}
+        />
+        <Route path="/circle-home" exact component={CircleHome} />
+        <Route
+          path="/mobilizer-profile2:circlename"
+          exact
+          component={MobilizerProfile}
+        />
+        <Route path="/circle-updates" exact component={CircleUpdates} />
+        <Route path="/circle-videos" exact component={CircleVideos} />
+        <Route
+          path="/circle-interactions"
+          exact
+          component={CircleInteractions}
+        />
+        <Route
+          path="/CircleDashboard:circlename"
+          exact
+          component={CircleDashboard}
+        />
+        <Route path="/hero-team" exact component={HeroTeam} />
+        <Route path="/echo:circlename" exact component={Echo} />
+        <Route
+          path="/account-information"
+          exact
+          component={AccountInformation}
+        />
+        <Route path="/edit-password" exact component={EditPassword} />
+        <Route
+          path="/welcome-circle:circlename"
+          exact
+          component={WelcomeCircles}
+        />
 
-        <Route path="/subscriptions" exact component={Subscriptions} />   
+        <Route path="/subscriptions" exact component={Subscriptions} />
+        <Route path="/login1" exact component={Login1} />
       </Router>
     </div>
   );
