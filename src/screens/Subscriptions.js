bcc1854432205eca67fe6ac890594c4ec5766027
takwa <takouaguetat@gmail.com>
2022-09-22 08:40:35 +0100

    fix subscriptions

--- src/screens/Subscriptions.js
+++ src/screens/Subscriptions.js
@@ -6,7 +6,26 @@ import "./Subscriptions.css";
 
 function Subscriptions(props) {
   const history = useHistory();
-
+  const subscriptions = [
+    {
+      circleName: "HERO First Circle",
+      subscriptionType: "HERO Subscription Type",
+      amount: "10",
+      image: "assets/img/sample/photo/1.jpg",
+    },
+    {
+      circleName: "HERO Second Circle",
+      subscriptionType: "HERO Subscription Type",
+      amount: "20",
+      image: "assets/img/sample/photo/1.jpg",
+    },
+    {
+      circleName: "HERO Third Circle",
+      subscriptionType: "HERO Subscription Type",
+      amount: "50",
+      image: "assets/img/sample/photo/1.jpg",
+    },
+  ];
   return (
     <>
       <Header
@@ -18,7 +37,7 @@ function Subscriptions(props) {
         whiteMode
         backTo={"/"}
       />
-      <div id="appCapsule" className="edit-password">
+      <div id="appCapsule" className="subscriptions">
         <div className="mt-4">
           <ion-icon
             src="assets/img/svg/account.svg"
@@ -46,6 +65,60 @@ function Subscriptions(props) {
               <br /> Circle subscriptions.
             </span>
             <hr className="hr mt-4 mb-4" />
+            <div className="section" style={{ width: "100%" }}>
+              <ul className="listview image-listview transparent flush">
+                {subscriptions.map((item) => (
+                  <li>
+                    <div className="item pe-0 pl-0">
+                      <img
+                        src={item.image}
+                        alt="image"
+                        className="imaged rounded w48 me-2"
+                      />
+                      <div className="in">
+                        <div
+                          className="flex-col"
+                          style={{ alignItems: "flex-start" }}
+                        >
+                          <strong>{item.circleName}</strong>
+                          <p>{item.subscriptionType}</p>
+                          <div className="mt-2">
+                            <button
+                              id="joinCircleBtn"
+                              type="button"
+                              className="btn btn-primary rounded font-size-btn me-1"
+                            >
+                              Modify
+                            </button>
+                            <button
+                              id="joinCircleBtn"
+                              type="button"
+                              className="btn btn-primary rounded font-size-btn"
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        </div>
+                        <div>
+                          <div className="amount-div black mt-1">
+                            <sup>€</sup>
+                            {item.amount}
+                            <span>/mo</span>
+                          </div>
+                          <button
+                            id="joinCircleBtn"
+                            type="button"
+                            className="btn btn-primary rounded font-size-btn mt-1"
+                          >
+                            Invoices
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
