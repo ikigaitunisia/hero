3a94fcdc69db855cc704b4b7bdb54ba81eb04262
hatem <hatemazaiez1@gmail.com>
2022-09-25 12:31:59 +0100

    ddd

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -49,11 +49,10 @@ function Menu(props) {
   }, [props.show]);
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
-    gapi.load('auth2', function() {
        gapi.auth2.init({
         clientId: clientId,
       });
-    });
+    
     if (user != null) {
       setLogedin(true);
       
