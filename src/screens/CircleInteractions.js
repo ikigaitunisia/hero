56d94df8a4a86b4a79c60a83e4c53dee4760d524
takwa <takouaguetat@gmail.com>
2022-09-26 16:32:02 +0100

    fix interactions

--- src/screens/CircleInteractions.js
+++ src/screens/CircleInteractions.js
@@ -37,7 +37,53 @@ function CircleInteractions(props) {
           ></ion-icon>
         </div>
         <div className="section mt-2">
-          <div className="card"></div>
+          <div className="card flex-center">
+            <img
+              src={"assets/img/logo3.png"}
+              alt="logo"
+              className="logo imaged w36 mt-3"
+            />
+            <p className="header-text mt-3 blue mb-1">
+              We will update you when the
+              <br /> first Livestream is planned.
+            </p>
+            <img
+              src={"assets/img/image10.png"}
+              alt="logo"
+              className="logo imaged w180 mt-3 mb-3"
+            />
+            <span>
+              Do you have questions or topics that you
+              <br /> would likes us to address in our next stream?
+            </span>
+            <form className="flex-center flex-col">
+              <div class="form-group boxed">
+                <div class="input-wrapper">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="text4b"
+                    placeholder="Leave your comment or questions here"
+                  />
+                  <i class="clear-input">
+                    <ion-icon
+                      name="close-circle"
+                      role="img"
+                      class="md hydrated"
+                      aria-label="close circle"
+                    ></ion-icon>
+                  </i>
+                </div>
+              </div>{" "}
+              <button
+                id="blueBtn"
+                type="button"
+                className="btn btn-primary rounded font-size-btn mt-2 mb-4 "
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
