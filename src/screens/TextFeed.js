c9acddb963071b794f7b239a4553d7bf14aafc4d
takwa <takouaguetat@gmail.com>
2022-09-28 10:47:36 +0100

    fix last commit

--- src/screens/TextFeed.js
+++ src/screens/TextFeed.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import Header from "../components/Header";
 import { withRouter } from "react-router-dom";
 import "./CircleUpdates.css";
-import axios from "axios";
 
 function TextFeed(props) {
   const history = useHistory();
-  const [item,setItem] = useState(props.item);
-  const [feed,setFeed] = useState([]);
-  const [feedHtml,setFeedHtml] = useState([]);
-  const [i,setI] = useState(props.index);
-  
-const like = (i,postID) => {
+  const [item, setItem] = useState(props.item);
+  const [feed, setFeed] = useState([]);
+  const [feedHtml, setFeedHtml] = useState([]);
+  const [i, setI] = useState(props.index);
+
+  const like = (i, postID) => {
     /*console.log("llll");
     axios.post("https://hegemony.donftify.digital:8080/supporter/react-post", {type:"LIKE",postId:postID,email:user.Email}, {
     }).then(res => {
@@ -22,64 +20,52 @@ const like = (i,postID) => {
       setFeed(feed);
     })*/
     console.log("ok");
-}
-useEffect(() => {
-
-  
-  
-
-
-
-})
+  };
+  useEffect(() => {});
   return (
     <>
-          <li>
-          <div>
-            <img
-              src="assets/img/sample/photo/1.jpg"
-              alt="image"
-              class="imaged w48 rounded"
-            />
-          </div>
-          <div
-            className="in"
-            style={{ textAlign: "start", marginLeft: "20px" }}
-          >
-            <div className="blue">
-              <div className="">
-                <strong>
-                UK Climate Justice Circle
-                </strong>
-              </div>
-              <div className="mb-05">
-                <span>@UK Climate Justice Circle</span>
-              </div>
-              <div className="text-xsmall">
-              Welcome to the Circle. We are thrilled to have you amongst our supporters. Stay tuned for updates about the projects we are working on !
-
-              </div>
+      <li>
+        <div>
+          <img
+            src="assets/img/sample/photo/1.jpg"
+            alt="image"
+            className="imaged w48 rounded"
+          />
+        </div>
+        <div className="in" style={{ textAlign: "start", marginLeft: "20px" }}>
+          <div className="blue">
+            <div className="">
+              <strong>UK Climate Justice Circle</strong>
             </div>
-            <div className="flex-row mt-3" key={i}>
-              <div className="flex-center flex-row" >
-                <ion-icon
-                  src="assets/img/svg/icon20.svg"
-                  style={{ width: "15px", height: "15px" }}
-                  class="me-1"
-                ></ion-icon>
-                <span className="me-2">0</span>
-              </div>
-              <div className="flex-center flex-row">
-                <ion-icon
-                  src="assets/img/svg/icon21.svg"
-                  style={{ width: "15px", height: "15px" }}
-                  class="me-1"
-                ></ion-icon>
-                <span>0</span>
-              </div>
+            <div className="mb-05">
+              <span>@UK Climate Justice Circle</span>
+            </div>
+            <div className="text-xsmall">
+              Welcome to the Circle. We are thrilled to have you amongst our
+              supporters. Stay tuned for updates about the projects we are
+              working on !
             </div>
           </div>
-          </li>
-        
+          <div className="flex-row mt-3" key={i}>
+            <div className="flex-center flex-row">
+              <ion-icon
+                src="assets/img/svg/icon20.svg"
+                style={{ width: "15px", height: "15px" }}
+                class="me-1"
+              ></ion-icon>
+              <span className="me-2">0</span>
+            </div>
+            <div className="flex-center flex-row">
+              <ion-icon
+                src="assets/img/svg/icon21.svg"
+                style={{ width: "15px", height: "15px" }}
+                class="me-1"
+              ></ion-icon>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </li>
     </>
   );
 }
